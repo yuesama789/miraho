34e1a06d7b4c5872326fd6e9781ae5faa94daad0
Mira Ho <mira.ho@syzygy.de>
2025-11-17 17:47:20 +0100

    added hover to pageheader profile pic

--- src/components/ui/pageheader/Pageheader.tsx
+++ src/components/ui/pageheader/Pageheader.tsx
@@ -14,9 +14,9 @@ const PageHeader: React.FC = () => {
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true" data-component-line="235"><path d="M12 15V3"></path><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><path d="m7 10 5 5 5-5"></path></svg>
     );
 
-    const scrollIndicator = (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true" data-component-line="254"><path d="m6 9 6 6 6-6"></path></svg>
-    );
+    //const scrollIndicator = (
+    //    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true" data-component-line="254"><path d="m6 9 6 6 6-6"></path></svg>
+    //);
 
     return (
         <>
