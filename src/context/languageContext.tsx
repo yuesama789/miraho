45e1ee065714d3c8661cf40a50870ec960b95193
yuesama789 <84022757+yuesama789@users.noreply.github.com>
2026-01-12 15:40:56 +0100

    set german as default language

--- src/context/languageContext.tsx
+++ src/context/languageContext.tsx
@@ -10,7 +10,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export const LanguageProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [locale, setLocale] = useState<Locale>('en');
+    const [locale, setLocale] = useState<Locale>('de');
     const t = useMemo(() => getTranslation(locale), [locale]);
 
     return (
